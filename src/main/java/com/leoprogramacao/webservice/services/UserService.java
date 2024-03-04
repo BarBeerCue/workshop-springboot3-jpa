d93aab9f268424f81460511ef2c313cae37275ec
BarBeerCue <leonardobalek@gmail.com>
2024-03-04 12:31:11 +0100

    Service layer, component registration

---
+++ src/main/java/com/leoprogramacao/webservice/services/UserService.java
@@ -0,0 +1,27 @@
+package com.leoprogramacao.webservice.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.leoprogramacao.webservice.entities.User;
+import com.leoprogramacao.webservice.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+
+}
