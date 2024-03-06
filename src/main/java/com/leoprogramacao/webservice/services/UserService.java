07ee5bef6f167c4734c86e5bbe962eb28cfda907
BarBeerCue <leonardobalek@gmail.com>
2024-03-06 12:06:02 +0100

    exception handler and find by id

--- src/main/java/com/leoprogramacao/webservice/services/UserService.java
+++ src/main/java/com/leoprogramacao/webservice/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.leoprogramacao.webservice.entities.User;
 import com.leoprogramacao.webservice.repositories.UserRepository;
+import com.leoprogramacao.webservice.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
