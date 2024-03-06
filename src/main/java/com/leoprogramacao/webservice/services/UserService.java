04015753196b104a9a036e9335212a65ff9b595e
BarBeerCue <leonardobalek@gmail.com>
2024-03-06 16:41:42 +0100

    Exception handling update

--- src/main/java/com/leoprogramacao/webservice/services/UserService.java
+++ src/main/java/com/leoprogramacao/webservice/services/UserService.java
@@ -13,6 +13,8 @@ import com.leoprogramacao.webservice.repositories.UserRepository;
 import com.leoprogramacao.webservice.services.exceptions.DatabaseException;
 import com.leoprogramacao.webservice.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 	
@@ -43,9 +45,13 @@ public class UserService {
 	}
 	
 	public User update(Long id, User obj) {
+		try {
 		User entity = repository.getReferenceById(id);
 		updateData(entity, obj);
 		return repository.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateData(User entity, User obj) {
